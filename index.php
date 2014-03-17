f0d496af71bc63ce1674eb1382f759c53bc21381
luukluuk34 <luuk-bartels@hotmail.com>
2014-03-17 09:54:22 +0100

    begin van de webpagina

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Knowledge database.</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="home">
+			<h1>Home</h1>
+			
+		</div>
+		<div class="top">
+			<img src="">
+			<button>Home</button>
+			<select>
+				<option style="display:none;" >Tutorials</option>
+				<option>HTML</option>
+				<option>CSS</option>
+				<option>Jquery</option>
+				<option>PHP</option>
+			</select>
+
+			<select>
+				<option style="display:none;">Blogs</option>
+				<option>HTML</option>
+				<option>CSS</option>	
+				<option>Jquery</option>
+				<option>PHP</option>
+			</select>
+
+			<button>Contact</button>
+
+
+		</div>
+
+	</div>
+
+
+</body>
+</html>
