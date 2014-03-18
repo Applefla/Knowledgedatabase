5f19389a385422911c4c6c08660e9d48460d032e
luukluuk34 <luuk-bartels@hotmail.com>
2014-03-18 13:50:00 +0100

    opvullogo, stylesheet en index.php gemaakt
    Index.php versie 1 is gemaakt kleuren en eventuele dingen kunnen nog
    veranderd worden.. de stylesheet is ook gemaakt daar kan ook
    veranderingen in worden geplaatst dit logo moet de plek invullen tot het
    officiële logo klaar is.

--- index.php
+++ index.php
@@ -1,39 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Knowledge database.</title>
+	<title>Applefla</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
 	<div class="wrapper">
+
 		<div class="home">
+			<input class="search" type="text">
 			<h1>Home</h1>
-			<input type="text">
 		</div>
-		<div class="top">
-			<img src="">
-			<button>Home</button>
-			<select>
-				<option style="display:none;" >Tutorials</option>
-				<option>HTML</option>
-				<option>CSS</option>
-				<option>Jquery</option>
-				<option>PHP</option>
-			</select>
+	
+		<img class="logo" src="./img/logo.png">
+		<div class="kiezen">
+			<button class="btn" id="home-button">Home</button>
 
-			<select>
-				<option style="display:none;">Blogs</option>
-				<option>HTML</option>
-				<option>CSS</option>	
-				<option>Jquery</option>
-				<option>PHP</option>
-			</select>
+			<button class="btn" id="tutorial">Tutorials</button>
 
-			<button>Contact</button>
+			<button class="btn" id="blog">Blog</button>
 
-
+			<button class="btn" id="contact">Contact</button>
 		</div>
-
+		<div>
+			<h3 class="align">Tutorial</h3>
+			<button class="first">HTML</button>
+			<button class="btn_query">CSS</button>
+			<button class="btn_query">JQUERY</button>
+			<button class="btn_query">PHP</button>
+		</div>
+		<div>
+			<h3 class="align">Blog</h3>
+			<button class="first">HTML</button>
+			<button class="btn_query">CSS</button>
+			<button class="btn_query">JQUERY</button>
+			<button class="btn_query">PHP</button>
+		</div>
+		<p class="copyright">Copyright &copy; Disclaimer-contact</p>
 	</div>
+
 
 
 </body>
